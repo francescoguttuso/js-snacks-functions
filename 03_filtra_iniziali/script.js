9fb537e43fa33837d5051a030bb03d73e3bf1193
francesco-guttuso <francescoguttuso@gmail.com>
2026-05-21 00:00:47 +0200

    Implement filtroParola function to filter words by initial letter

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -4,10 +4,26 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 
 // Dichiara la funzione qui.
+function filtroParola (lettere, lettereDaCercare) {
+    const parole = [];
+    
+    for (let i = 0; i < lettere.length; i++) {
+        const parola = lettere[i];
+   
+    const inizialeParola = parola[0];
+
+if (inizialeParola === lettereDaCercare) {
+    parole.push(parola);
+        } 
+    }
+    return parole;
+}
+
 
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+ const filtroLettereA = filtroParola (names,"A");
+ console.log(filtroLettereA);
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
