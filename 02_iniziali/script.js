fbab29da8c19ce24e93e026354a84bd28bbf3ca9
francesco-guttuso <francescoguttuso@gmail.com>
2026-05-20 23:35:43 +0200

    Implement iniziali function to extract initials from an array of names

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -5,9 +5,19 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 // Dichiara la funzione qui.
 
-
+function iniziali(lettere){
+    const inizialiParola = [];
+    
+    for (let i = 0; i <lettere.length; i++){
+        const parola = lettere[i];
+        const iniziale = parola[0];
+        inizialiParola.push(iniziale);
+    }
+    return inizialiParola
+}
 // Invoca la funzione qui e stampa il risultato in console
 
-
+const inizialiParole = iniziali (names);
+console.log(inizialiParole);
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
